f16c822696ea29582c65d2ae7a41293d65b51882
darchdekin <darchdekin02@gmail.com>
2024-04-14 19:56:46 -0500

    added header and footer components
    they don't have any content yet though

--- frontend/mizYDSA/src/app/app.component.ts
+++ frontend/mizYDSA/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderBarComponent } from './header-bar/header-bar.component';
 
 @Component({
   selector: 'app-root',
--- frontend/mizYDSA/src/app/app.module.ts
+++ frontend/mizYDSA/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderBarComponent } from './header-bar/header-bar.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderBarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
