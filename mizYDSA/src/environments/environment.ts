8bc2727f42a2f0d68742855ad194002bbff1029b
darchdekin <darchdekin02@gmail.com>
2024-05-03 13:45:50 -0500

    frontend can make api calls to local server

--- mizYDSA/src/environments/environment.ts
+++ mizYDSA/src/environments/environment.ts
@@ -1,6 +1,7 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://localhost:3000/api'
-    //apiUrl: 'http://52.207.241.126:3000'
+    //apiUrl: 'http://localhost:3000/api'
+    //apiUrl: 'http://54.227.157.207:3000/api'
+    apiUrl: '/api'
   };
   
